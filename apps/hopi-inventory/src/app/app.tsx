f3d6490fe9d0cf995f3c7f0f03606143903feafe
roto93 <roto93@yahoo.com.tw>
2023-10-29 13:02:14 +0800

    :fire: FE: remove template files

--- apps/hopi-inventory/src/app/app.tsx
+++ apps/hopi-inventory/src/app/app.tsx
@@ -1,12 +1,26 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 
-import NxWelcome from './nx-welcome';
-
 export function App() {
+
+  const onSubmit = async () => {
+    const res = await fetch('http://localhost:3333/api')
+    const json = await res.json()
+    console.log(json)
+  }
+
   return (
     <div>
-      <NxWelcome title="hopi-inventory" />
+      <form onSubmit={onSubmit}>
+        <label htmlFor="">
+          email
+          <input type="email" />
+        </label>
+        <label htmlFor="">
+          password
+          <input type="password" />
+        </label>
+        <button type='button' onClick={onSubmit}>submit</button>
+      </form>
     </div>
   );
 }
