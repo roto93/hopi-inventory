cef058643a62a6936635bf2d476dc1a3f876d1ea
roto93 <roto93@yahoo.com.tw>
2023-11-05 16:45:24 +0800

    :zap: (FE) dynamic header nav bar for logged in or not

--- apps/hopi-inventory/src/pages/home/components/header/Header.tsx
+++ apps/hopi-inventory/src/pages/home/components/header/Header.tsx
@@ -1,12 +1,23 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 import styles from './header.module.scss'
 
 const Header = () => {
+  const { pathname } = useLocation()
+
   return (
     <header className={styles.container}>
       <nav className={styles.nav}>
-        <NavLink to={'/'}>Home</NavLink>
-        <NavLink to={'/login'}>Login</NavLink>
+        {
+          pathname === '/login' || pathname === '/signup'
+            ? <>
+              <NavLink to={'/login'}>Log in</NavLink>
+              <NavLink to={'/signup'}>Sign up</NavLink>
+            </>
+            : <>
+              <NavLink to={'/'}>Home</NavLink>
+              <NavLink to={'/user'}>User</NavLink>
+            </>
+        }
       </nav>
     </header>)
 }
