24db7104350024e0471484f05a17d156e8edf1d2
roto93 <roto93@yahoo.com.tw>
2023-11-05 16:58:19 +0800

    :bug: (FE) fix path alias

--- apps/hopi-inventory/vite.config.ts
+++ apps/hopi-inventory/vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import path from 'path';
 
 export default defineConfig({
   cacheDir: '../../node_modules/.vite/hopi-inventory',
@@ -30,5 +31,10 @@ export default defineConfig({
     },
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 });
