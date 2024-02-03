5fe73cfaaaec3e9710b22132fd2f91c0b592e04f
roto93 <roto93@yahoo.com.tw>
2024-02-03 16:56:16 +0800

    :zap: (BE) persist login sessions when server restarts

--- apps/hopi-inventory-api/src/main.ts
+++ apps/hopi-inventory-api/src/main.ts
@@ -9,7 +9,7 @@ import init from './configs/passportConfig';
 import authRoutes from './routes/authRoutes';
 import mongoose from 'mongoose'
 import userRoutes from './routes/userRoutes';
-
+import MongoStore from 'connect-mongo'
 
 if (process.env.NODE_ENV !== 'production') {
   dotenv.config()
@@ -17,38 +17,63 @@ if (process.env.NODE_ENV !== 'production') {
 
 
 const app = express();
+
+
+// allow to server to accept request from different origin
 app.use(cors({
-  origin: "http://localhost:4200", // allow to server to accept request from different origin
+  origin: "http://localhost:4200", 
   methods: "GET,PUT,PATCH,POST,DELETE",
   credentials: true
 }))
-app.use(express.json()) // json parser
+
+
+// json parser
+app.use(express.json()) 
+
+
+// client can access static files at /assets
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-app.use(flash())
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
-}))
 
-init(passport)
-app.use(passport.initialize())
-app.use(passport.session())
-
-
-// Routes
-app.use('/auth', authRoutes)
-app.use('/user', userRoutes)
-
-
+// If no database name is specified in the connection string,
+// MongoDB will default to using a database named "test".
+// If "test" doesn't exist, MongoDB will create it when saving a document.
+// 
+// Alternatively, specify a different database name in the connection string:
+// const uri = 'mongodb+srv://<username>:<password>@<clustername>.mongodb.net/mydatabase';
 mongoose.connect(process.env.MONGO_URL)
-  .then(() => {
+  .then((m) => {
 
+    // set up sessions, persist sessions when server restart
+    app.use(flash())
+    app.use(session({
+      secret: 'test',
+      resave: true,
+      store: MongoStore.create({ client: mongoose.connection.getClient() as any }),
+      saveUninitialized: true,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 3, // 设置 session 的有效时间，单位毫秒
+      },
+    }));
+
+
+    // use passport.js
+    init(passport)
+    app.use(passport.initialize())
+    app.use(passport.session())
+
+
+    // Routes
+    app.use('/auth', authRoutes)
+    app.use('/user', userRoutes)
+
+
+    // start listening
     const port = process.env.PORT || 3333;
     const server = app.listen(port, () => {
       console.log(`Connecting to database and listening at http://localhost:${port}/api`);
     });
     server.on('error', console.error);
+
   })
   .catch(e => console.error(e))
